50dad8292d537d4096804d6c4140808e61afa1fc
Charles Doebler <cdoebler1@gmail.com>
2021-02-03 15:40:00 -0500

    fix

--- __init__.py
+++ __init__.py
@@ -29,20 +29,20 @@ class Chatbot(MycroftSkill):
 
     chatting = False
 
-    #def __init__(self):
-    #    super(Chatbot, self).__init__(name='Winston_Chatbot')
-    self.kernel = aiml.Kernel()
+    def __init__(self):
+        super(Chatbot, self).__init__(name='Winston_Chatbot')
+        self.kernel = aiml.Kernel()
 #        chatbot_brain = self.settings.get('chatbot_brain')
-    chatbot_brain = "AnnaL"
-    self.aiml_path = os.path.join(dirname(__file__), chatbot_brain)
-    self.brain_path = os.path.join(self.file_system.path, 'bot_brain.brn')
+        chatbot_brain = "AnnaL"
+        self.aiml_path = os.path.join(dirname(__file__), chatbot_brain)
+        self.brain_path = os.path.join(self.file_system.path, 'bot_brain.brn')
         # reloading skills will also reset this 'timer', so ideally it should
         # not be too high
-    self.line_count = 1
-    self.save_loop_threshold = int(self.settings.get('save_loop_threshold',
+        self.line_count = 1
+        self.save_loop_threshold = int(self.settings.get('save_loop_threshold',
                                                          4))
 
-    self.brain_loaded = False
+        self.brain_loaded = False
 
     def load_brain(self):
         """Set up the aiml engine using available device information."""
@@ -138,8 +138,8 @@ class Chatbot(MycroftSkill):
 
     def converse(self, utterances, lang="en-us"):
         if self.chatting:
-            if self.voc_match(utterances[0], "StopKeyword") and self.voc_match(utterances[0], "ChatKeyword"):
-                return False
+#            if self.voc_match(utterances[0], "StopKeyword") and self.voc_match(utterances[0], "ChatKeyword"):
+#                return False
 
             if not self.brain_loaded:
                 self.load_brain()
